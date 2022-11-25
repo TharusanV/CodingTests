674a29f3194076b0e59b49c532cc9c7b1fbaf32c
Tharusan Vijayakumar <tharusan.vijayakumar@gmail.com>
2022-11-25 17:17:53 +0000

    Codility_ - Lesson 2 (Arrays)

--- src/tests/CyclicRotation.java
+++ src/tests/CyclicRotation.java
@@ -1,10 +1,20 @@
 package tests;
 
 public class CyclicRotation {
+	
+	public int[] solution(int[] A, int K) {
+		for(int i=0; i<K; i++) {
+			int lastPosValue = A[A.length-1];
+            for(int j = A.length-1; j > 0; j--){        
+                A[j] = A[j-1];    
+            }
+            A[0] = lastPosValue;
+		}
+		return A;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
---
+++ src/tests/OddOccurencesInArray.java
@@ -0,0 +1,30 @@
+package tests;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class OddOccurencesInArray {
+	
+	public static int solution(int[] A) {
+		ArrayList<Integer> leftOver = new ArrayList<>();
+		
+		for(int j=0; j<A.length; j++) {
+			int value = A[j];
+			if(leftOver.contains(value)){
+				leftOver.remove(new Integer(value));
+			}
+			else {
+				leftOver.add(value);
+			}
+
+		}
+		return leftOver.get(0);
+	}
+
+	public static void main(String[] args) {
+		int[] arrayEx = {1,1,3,2,3};
+		System.out.println(solution(arrayEx));
+
+	}
+
+}
